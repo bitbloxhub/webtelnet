ab2828b9cb7ae813ce3697a63118f70780947a1a
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2021-11-08 09:10:41 -0800

    init

---
+++ webtelnet/__init__.py
@@ -0,0 +1,29 @@
+import asyncio
+from sanic import Sanic
+
+__version__ = '0.1.0'
+
+app = Sanic("webtelnet")
+
+app.config["THOST"] = "telehack.com"
+app.config["TPORT"] = 23
+
+app.static("/", "webtelnet/index.html")
+
+async def websocket_fwd2client(ws, rsreader):
+    while True:
+        tosend = await rsreader.read(1024)
+        await ws.send(tosend)
+
+async def websocket_fwd2serv(ws, rswriter):
+    while True:
+        recvd = await ws.recv()
+        rswriter.write(recvd.encode())
+        await rswriter.drain()
+
+@app.websocket("/ws")
+async def websocket(req, ws):
+    rsreader, rswriter = await asyncio.open_connection(app.config["THOST"], app.config["TPORT"])
+    asyncio.create_task(websocket_fwd2client(ws, rsreader))
+    asyncio.create_task(websocket_fwd2serv(ws, rswriter))
+    await asyncio.Future()
---
+++ webtelnet/__main__.py
@@ -0,0 +1,2 @@
+from webtelnet import app
+app.run(host='0.0.0.0', port=8080, workers=8, access_log=False)
